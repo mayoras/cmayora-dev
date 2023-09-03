ef31c3a9629021779be4e61827366ed7957fe93e
Cesar Mayora <cesarmay@protonmail.com>
2023-09-03 22:19:20 +0200

    feat(navlink): navLink component

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,16 @@
+import { type FC } from 'react';
+
+interface LinkProps {
+  label: string;
+  to: string;
+}
+
+const NavLink: FC<LinkProps> = ({ label, to }) => {
+  return (
+    <div className="cursor-pointer transition-colors hover:text-purple-500">
+      <a href={to}>{label}</a>
+    </div>
+  );
+};
+
+export default NavLink;
