a778f7bbba75ff9829ff2bc13951ba0341c91639
Cesar Mayora <cesarmay@protonmail.com>
2023-09-03 18:30:02 +0200

    fix(meta): update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.cmayora.dev/',
   integrations: [react(), tailwind()]
 });
