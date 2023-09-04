7d6e8edb82fb324860f2f629c3ea29c0cbaaedcb
Cesar Mayora <cesarmay@protonmail.com>
2023-09-04 17:12:29 +0200

    refactor(langselector): dark top border
    lighten a bit the top border color in dark mode

--- src/components/LangSelector.tsx
+++ src/components/LangSelector.tsx
@@ -20,7 +20,7 @@ const LangSelector: FC<LangSelectorProps> = () => {
       <label className="lang-icon">
         <IoLanguageOutline className="pointer-events-none absolute z-10 ml-2 mt-[10px] inline-block text-gray-600 dark:text-cyan-300" />
       </label>
-      <select className="relative w-full cursor-pointer text-ellipsis rounded-md border border-slate-400 py-3 pl-10 pr-14 text-lg text-gray-600 transition-colors ease-in hover:border-black hover:text-black dark:border-slate-600 dark:bg-slate-600 dark:text-slate-300 dark:hover:border-slate-50 dark:hover:text-slate-50">
+      <select className="relative w-full cursor-pointer text-ellipsis rounded-md border border-slate-400 py-3 pl-10 pr-14 text-lg text-gray-600 transition-colors ease-in hover:border-black hover:text-black dark:border-slate-600 dark:border-t-slate-500 dark:bg-slate-600 dark:text-slate-300 dark:hover:border-slate-50 dark:hover:text-slate-50">
         {LANGS.map(lang => {
           return (
             <option key={lang.key} value={lang.key}>
