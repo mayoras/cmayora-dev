88fec652e41dbe5ba693224830bf4f2db3165c57
Cesar Mayora <cesarmay@protonmail.com>
2023-09-04 02:01:09 +0200

    feat(themetoggler): toggle dark and light theme

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,13 +1,54 @@
-import { type FC } from 'react';
+import { type FC, useState, useEffect } from 'react';
 import { IoSunny } from 'react-icons/io5';
 
 interface ThemeTogglerProps {}
 
 const ThemeToggler: FC<ThemeTogglerProps> = () => {
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined;
+    }
+
+    if (
+      typeof localStorage !== 'undefined' &&
+      localStorage['theme'] !== 'undefined'
+    ) {
+      return localStorage['theme'];
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+
+    // default light theme
+    return 'light';
+  });
+
+  function toggleTheme() {
+    const toggled = theme === 'light' ? 'dark' : 'light';
+
+    // update localStorage
+    localStorage.setItem('theme', toggled);
+
+    // toggle theme
+    setTheme(toggled);
+  }
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if (theme === 'light') {
+      root.classList.remove('dark');
+    } else {
+      root.classList.add('dark');
+    }
+  }, [theme]);
+
   return (
-    <div className="nav-theme-toggler cursor-pointer text-3xl text-slate-600 transition-colors ease-in hover:text-black dark:text-white dark:hover:text-slate-400">
+    <button
+      onClick={toggleTheme}
+      className="nav-theme-toggler cursor-pointer text-3xl text-slate-600 transition-colors ease-in hover:text-black dark:text-white dark:hover:text-slate-400"
+    >
       <IoSunny />
-    </div>
+    </button>
   );
 };
 
